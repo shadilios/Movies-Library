7d7b197244a437639568f62bd4b22e0234e236dd
shadilios <shadilios@hotmail.com>
2022-02-23 00:14:15 +0200

    added task13 solution, not complete

---
+++ schema.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS favoriteMoviesTable
+
+CREATE TABLE IF NOT EXISTS favoriteMoviesTable(
+
+  
+  id SERIAL PRIMARY KEY,    
+  title VARCHAR(255),
+  release_date INTEGER,
+  poster_path VARCHAR(1000),
+  overview VARCHAR(10000),   
+  
+
+);
