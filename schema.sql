3953308bf18c190362e53d3742bb332bdd32b6b1
shadilios <shadilios@hotmail.com>
2022-02-26 22:10:46 +0300

    Task finally done for the whole week v1

--- schema.sql
+++ schema.sql
@@ -1,13 +1,10 @@
 DROP TABLE IF EXISTS favoriteMoviesTable
 
 CREATE TABLE IF NOT EXISTS favoriteMoviesTable(
-
-  
   id SERIAL PRIMARY KEY,    
   title VARCHAR(255),
-  release_date INTEGER,
+  release_date VARCHAR(255),
   poster_path VARCHAR(1000),
   overview VARCHAR(10000),   
-  
-
+  comment VARCHAR(1000)
 );
